ace26caee369a31fcf1ed19a534741544366bfd7
Luca Galli <lucagalli05@gmail.com>
2025-09-20 21:53:50 +0200

    Tried to fix error vercel

--- components/PrimaryButton.tsx
+++ components/PrimaryButton.tsx
@@ -1,5 +1,16 @@
-const PrimaryButton = ({ children }) => {
-    return <button className="primary-button">{children}</button>;
+import { ReactNode } from "react";
+
+type PrimaryButtonProps = {
+    children: ReactNode;
+    onClick?: () => void; // optional click handler
+};
+
+const PrimaryButton = ({ children, onClick }: PrimaryButtonProps) => {
+    return (
+        <button className="primary-button" onClick={onClick}>
+            {children}
+        </button>
+    );
 };
 
 export default PrimaryButton;
