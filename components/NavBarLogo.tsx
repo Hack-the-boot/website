ba5ba7a45cea74eac564f58e61b053ec4ef071b7
Luca Galli <lucagalli05@gmail.com>
2025-09-20 21:48:45 +0200

    Added text for error

--- components/NavBarLogo.tsx
+++ components/NavBarLogo.tsx
@@ -1,19 +1,19 @@
 import PrimaryButton from "./PrimaryButton";
 import SecondaryButton from "./SecondaryButton";
 const NavBar = () => {
-  return (
-    <nav className="nav-bar-no-logo ">
-      <a href="">Event</a>
-      <a href="">Who We Are</a>
-      <a href="">Sponsors</a>
-      <a href="">FAQs</a>
-      <a href="">Contact Us</a>
-      <div className="ml-110">
-        <PrimaryButton />
-      </div>
-      <SecondaryButton />
-    </nav>
-  );
+    return (
+        <nav className="nav-bar-no-logo ">
+            <a href="">Event</a>
+            <a href="">Who We Are</a>
+            <a href="">Sponsors</a>
+            <a href="">FAQs</a>
+            <a href="">Contact Us</a>
+            <div className="ml-110">
+                <PrimaryButton> Register</PrimaryButton>
+            </div>
+            <SecondaryButton />
+        </nav>
+    );
 };
 
 export default NavBar;
